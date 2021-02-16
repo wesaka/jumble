2f472c90e98196c6813054c7f5366d61e336a004
Wesley Kalatai <kalatai.alberti@gmail.com>
2021-02-16 16:45:36 -0300

    Found a better way to find the bonus answers. Deleted the bonus_answer file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from bonus_answer import bonus_answer
+from itertools import permutations
 
 word_dict = json.load(open('words_dictionary.json'))
 
@@ -18,23 +18,49 @@ def solve(letters):
     return possible_words
 
 
+def solve_inexact(letters, current_len):
+    # The next step is to find all the permutations with the length equal to the word, inside the letters given
+    perms = permutations(letters, current_len)
+
+    # Lets also create a "blacklist" for all the possibilities discarded, so the program doesn't waste our time
+    blacklist = list()
+
+    for perm in perms:
+        current_perm = ''.join(perm)
+        possibilities = solve(current_perm)
+
+        for possibility in possibilities:
+            if possibility in blacklist:
+                continue
+
+            answer = input('Is ' + possibility + ' the correct word? (y/n)')
+            if answer == 'y':
+                # The next step is to remove the characters that form the accepted word
+                for c in perm:
+                    letters = letters.replace(c, '', 1)
+
+                return possibility, letters
+
+            else:
+                blacklist.append(possibility)
+
+
 if __name__ == '__main__':
     isStop = 'y'
     while isStop == 'y':
         print(solve(input('Write the letters you wanna check against:').lower()))
 
-        isStop = input('Do you want to search another word? (y/n)').lower()
+        isStop = input('Do you want to search another word? (y/N)').lower()
 
         if isStop != 'n' and isStop != 'y' and isStop != 'bonus':
             print('Option not recognized. Exiting')
             exit(0)
 
     if isStop == 'bonus':
-        bonus_letters = input('Enter the bonus letters:')
-        number_of_words = int(input('Enter the number of words:'))
-        word_length = list()
-
+        bonus_letters = input('Enter the letters of the bonus answer: ').lower()
+        number_of_words = int(input('And now, tell me how many words there are to solve: '))
         for i in range(0, number_of_words):
-            word_length.append(int(input('Insert the length of the word number ' + str(i + 1))))
+            current_len = int(input('Now, what is the length of the word #' + str(i + 1) + "? "))
+            answer, bonus_letters = solve_inexact(bonus_letters, current_len)
 
-        bonus_answer(bonus_letters, number_of_words, word_length)
+            print('The word #' + str(i + 1) + ' is: ' + answer + '\n')
